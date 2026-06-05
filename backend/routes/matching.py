50f6b703dc4a862287654fa2c4cd7ec1a9d8c851
TOBIE AMADOU <geduldamadou@gmail.com>
2026-06-05 06:28:07 +0100

    fix: corrige 7 bugs algorithme calculer_score

--- backend/routes/matching.py
+++ backend/routes/matching.py
@@ -1,31 +1,34 @@
 def calculer_score(mentor, mentore, disponibilites):
-	matieres_communes = set(mentor["points_forts"]) & set(mentore["points_faibles"])	total_besoins = len(mentore["points_faibles"])
-if total_besoins > 0 :
-		total_besoins = (len(matieres_communes) / total_besoins) * 60
-else :
-		total_besoins = 0
 
-	dispo_mentor = set(disponibilites.get(mentor["id"] , []))
-	dispo_mentore = set(disponibilites.get(mentore["id"] , []))
-	creneaux_communs = dispo_mentor & dispo_mentore
-	total_creneaux = len(dispo_mentore)
+    # COMPETENCES
+    matieres_communes = set(mentor["points_forts"]) & set(mentore["points_faibles"])
+    total_besoins = len(mentore["points_faibles"])
 
-	if total_creneaux > 0:
-		score_dispos = (len(creneaux_communs)/ total_creneaux) * 30
-	else:
-		
-		score_dispos = O
+    if total_besoins > 0:
+        score_competences = (len(matieres_communes) / total_besoins) * 60
+    else:
+        score_competences = 0
 
-	fillieres_proches =  {frozenset({"SI" , "GL"}), {frozenset({"IA" , "IM"})}
-	paire = frozenset({mentor["filiere"], mentore["filiere"]})
+    # DISPONIBILITES
+    dispo_mentor  = set(disponibilites.get(mentor["id"], []))
+    dispo_mentore = set(disponibilites.get(mentore["id"], []))
+    creneaux_communs = dispo_mentor & dispo_mentore
+    total_creneaux = len(dispo_mentore)
 
-	if mentor["filiere"] = mentore["filiere"]:
-		scrore_filiere = 10
-	elif paire in fillieres_proches :
-		score_filiere = 6
- 	else:
-		score_filiere = 0
+    if total_creneaux > 0:
+        score_dispos = (len(creneaux_communs) / total_creneaux) * 30
+    else:
+        score_dispos = 0
 
-	return round(min(score_competences + score_dispos + score_filiere, 100.0), 2)
+    # FILIERE
+    fillieres_proches = {frozenset({"SI", "GL"}), frozenset({"IA", "IM"})}
+    paire = frozenset({mentor["filiere"], mentore["filiere"]})
 
+    if mentor["filiere"] == mentore["filiere"]:
+        score_filiere = 10
+    elif paire in fillieres_proches:
+        score_filiere = 6
+    else:
+        score_filiere = 0
 
+    return round(min(score_competences + score_dispos + score_filiere, 100.0), 2)
